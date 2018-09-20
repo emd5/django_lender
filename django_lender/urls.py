810b3d030f4206421a23850a4bd757397f31c561
emd5 <emahoney5@mail.greenriver.edu>
2018-09-19 22:09:50 -0700

    redo HMAC implementation and testing

--- django_lender/urls.py
+++ django_lender/urls.py
@@ -19,6 +19,9 @@ from .views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', home_view, name='home'),
     path('books/', include('lender_books.urls'))
+
 ]
